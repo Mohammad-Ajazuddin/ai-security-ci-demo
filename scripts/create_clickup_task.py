0ff463aba2e1460401e2b153d3168265362da459
Mohammad Ajazuddin <mohd.ajazzuddin@gmail.com>
2026-01-30 00:34:09 +0530

    fix : clickup API

--- scripts/create_clickup_task.py
+++ scripts/create_clickup_task.py
@@ -1,15 +1,22 @@
 import requests
 import os
+from datetime import datetime
 
 CLICKUP_TOKEN = os.getenv("CLICKUP_TOKEN")
 LIST_ID = os.getenv("CLICKUP_LIST_ID")
 
+if not CLICKUP_TOKEN or not LIST_ID:
+    raise ValueError("Missing CLICKUP_TOKEN or CLICKUP_LIST_ID")
+
 with open("ai-summary.txt") as f:
     content = f.read()
 
-#url = f"https://api.clickup.com/api/v2/list/{LIST_ID}/task"
+# Generate timestamp
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-url = f"https://app.clickup.com/37403503/v/l/li/{LIST_ID}/task"
+task_name = f"🚨 Security Issues - {timestamp}"
+
+url = f"https://api.clickup.com/api/v2/list/{LIST_ID}/task"
 
 headers = {
     "Authorization": CLICKUP_TOKEN,
@@ -17,11 +24,12 @@ headers = {
 }
 
 payload = {
-    "name": "🚨 Security Issues Detected - CI Pipeline",
+    "name": task_name,
     "description": content,
     "priority": 1
 }
 
 response = requests.post(url, json=payload, headers=headers)
 
-print("ClickUp Task Status:", response.status_code)
+print("ClickUp Status Code:", response.status_code)
+print("ClickUp Task Name:", task_name)
